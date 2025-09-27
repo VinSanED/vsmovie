b73382cfaef7d22a14e32b613008347b01e90fd6
VinKSSan <vinsan.ed@proton.me>
2025-09-27 22:55:11 +0000

    add vsmovie-front, cleaning up unnecessary files and code

---
+++ vsmovie-front/src/App.tsx
@@ -0,0 +1,14 @@
+
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <>
+      <img width={150} alt="logo" src={logo} />
+      <h1>page initialize</h1>
+      <a>aad</a>
+    </>
+  );
+}
+
+export default App;
