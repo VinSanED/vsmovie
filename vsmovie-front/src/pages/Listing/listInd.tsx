c27918544dfd4454c82ac5a118750f412bbf30db
VinKSSan <vinsan.ed@proton.me>
2025-09-29 01:53:24 +0000

    initialize requests of a movies

--- vsmovie-front/src/pages/Listing/listInd.tsx
+++ vsmovie-front/src/pages/Listing/listInd.tsx
@@ -1,11 +1,22 @@
 import MovieCard from "components/movieCard/movieCarInd"
 import Pagination from "components/pagination/pagInd"
 import { GridContainer, ListSty } from "./listSty"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { BASE_URL } from "utils/request"
 
 export const Listing=()=>{
+    const [moviePage, setMoviePage] = useState({});
+    const [pageNumber, setPageNumber] = useState(0)
+
+    useEffect(()=>{
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+            .then(res=>setMoviePage(res.data))
+    },[pageNumber])
+
     return(
         <ListSty>
-            <Pagination/>
+            <Pagination pageNumber={pageNumber}/>
             <GridContainer className="conteiner">
                 <MovieCard/>
                 <MovieCard/>
